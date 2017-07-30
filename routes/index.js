126a2baf37871905b3667954a420742fff3cc815
Robert Brignull <robertbrignull@gmail.com>
2017-07-30 23:25:16 +0100

    Add index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  if (typeof req.query.code !== 'undefined') {
+      res.render('success', {});
+      return;
+  }
+
+  res.render('addToSlack', {
+      client_id: encodeURIComponent(process.env.CLIENT_ID),
+      redirect_uri: encodeURIComponent(process.env.URL)
+  });
 });
 
 module.exports = router;
