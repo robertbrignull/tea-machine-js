2083f3b9e49e4aac5f3456d8905bac92b0d410ef
Robert Brignull <robertbrignull@gmail.com>
2017-08-05 14:21:01 +0100

    use bodyParser instead of own solution

--- routes/brew.js
+++ routes/brew.js
@@ -4,39 +4,16 @@ var router = express.Router();
 const querystring = require('querystring');
 
 router.post('/', function(req, res) {
-    parseBody(req, res, function (body) {
-        if (body.token !== process.env.VERIFICATION_TOKEN) {
-            res.status(400);
-            res.send('Verification token incorrect');
-            return;
-        }
+    if (req.body.token !== process.env.VERIFICATION_TOKEN) {
+        res.status(400);
+        res.send('Verification token incorrect');
+        return;
+    }
 
-        const brewingMinutes = parseBrewingMinutes(body.text);
+    const brewingMinutes = parseBrewingMinutes(req.body.text);
 
-        res.send("Will remind you in " + brewingMinutes + " minutes");
-    })
+    res.send("Will remind you in " + brewingMinutes + " minutes");
 });
-
-function parseBody(req, res, callback) {
-    var rawBody = '';
-    req.on('data', function (data) {
-        rawBody += data;
-        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-        if (rawBody.length > 1e6) {
-            // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-            req.connection.destroy();
-            res.status(413);
-        }
-    });
-    req.on('end', function () {
-        const parsedBody = {};
-        for (const line in rawBody.split("\n")) {
-            const lineParts = line.split("=");
-            parsedBody[lineParts[0]] = lineParts[1];
-        }
-        callback(parsedBody);
-    });
-}
 
 function parseBrewingMinutes(text) {
     if (typeof text !== 'undefined' && text !== '')
